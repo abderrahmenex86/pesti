824cf17ce610b247bda07797c779cd88c99b7d40
BENOUNENE Abderrahmene <77858997+abderrahmenex86@users.noreply.github.com>
2026-05-13 06:51:28 +0100

    Update dataset to ditch hierarchical approach

--- dataset.py
+++ dataset.py
@@ -6,16 +6,18 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 
-class IP102HierarchicalDataset(Dataset):
-    def __init__(self, root_dir, taxonomy_map, transform=None, split="train"):
+class IP102Dataset(Dataset):
+    def __init__(self, root_dir, transform=None, split="train"):
+        assert split in [
+            "train",
+            "val",
+            "test",
+        ], "Invalid split. Expected one of ['train', 'val', 'test']"
         self.root_dir = root_dir
         self.transform = transform
-        self.taxonomy_map = taxonomy_map
         self.img_dir = os.path.join(root_dir, "images")
 
         splits = {"train": "train.txt", "val": "val.txt", "test": "test.txt"}
-        if split not in splits:
-            raise ValueError(f"Invalid split. Expected one of {list(splits.keys())}")
 
         labels_fname = os.path.join(root_dir, splits[split])
 
@@ -40,15 +42,6 @@ class IP102HierarchicalDataset(Dataset):
         else:
             raise ValueError("You must provide a transform.")
 
-        species_id = int(self.labels[idx])
+        target = torch.tensor(int(self.labels[idx]), dtype=torch.long)
 
-        order_id = self.taxonomy_map[str(species_id)]["order"]
-        family_id = self.taxonomy_map[str(species_id)]["family"]
-
-        targets = {
-            "order": torch.tensor(order_id, dtype=torch.long),
-            "family": torch.tensor(family_id, dtype=torch.long),
-            "species": torch.tensor(species_id, dtype=torch.long),
-        }
-
-        return image, targets
+        return image, target
